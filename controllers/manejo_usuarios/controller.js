56c1a3c5782cd9ad33af2dbe98a49942db2efcd4
David Cárdenas <santito500@live.com>
2024-03-05 20:24:34 -0500

    feat: :sparkles: Cookie Storing
    Storing cookies for the JWT token within the browser, ensuring the HttpOnly flag.

--- controllers/manejo_usuarios/controller.js
+++ controllers/manejo_usuarios/controller.js
@@ -1,31 +1,37 @@
 import axios from "axios";
 import { PORT, HOST } from "../../config.js";
 
-
 export const renderLogin = (req, res) => {
-    res.render("manejo_usuarios/login.ejs");
-}
+  res.render("manejo_usuarios/login.ejs");
+};
 
 export const userLogging = async (req, res) => {
-    const endpoint = "/logIn"
-    axios.post(`${HOST}:${PORT}${endpoint}`, { email: req.body.email, password: req.body.password });
-}
+  const endpoint = "/usuario/logIn";
+  axios
+    .post(`${HOST}:${PORT}${endpoint}`, {
+      email: req.body.email,
+      password: req.body.password,
+    })
+    .then((response) => {});
+};
 
 export const renderRegister = (req, res) => {
-
-    res.render("manejo_usuarios/register.ejs");
-}
-
+  res.render("manejo_usuarios/register.ejs");
+};
 
 export const register = async (req, res) => {
-    const endpoint = "/registro"
-    axios.post(`${HOST}:${PORT}${endpoint}`, {
-        nombre: req.body.nombre, apellido:
-            req.body.apellido, username: req.body.username, email: req.body.email,
-        metodospago: req.body.metodospago, numero_tarjeta:
-            req.body.numero_tarjeta, cvv: req.body.cvv,
-        fecha_exp: req.body.fecha_exp, pregunta_1: req.body.pregunta_1, pregunta_2: req.body.pregunta_2,
-        pregunta_3: req.body.pregunta_3
-
-    });
-}
+  const endpoint = "/usuario/registro";
+  axios.post(`${HOST}:${PORT}${endpoint}`, {
+    nombre: req.body.nombre,
+    apellido: req.body.apellido,
+    username: req.body.username,
+    email: req.body.email,
+    metodospago: req.body.metodospago,
+    numero_tarjeta: req.body.numero_tarjeta,
+    cvv: req.body.cvv,
+    fecha_exp: req.body.fecha_exp,
+    pregunta_1: req.body.pregunta_1,
+    pregunta_2: req.body.pregunta_2,
+    pregunta_3: req.body.pregunta_3,
+  });
+};
--- public/manejo_usuarios/js/login.js
+++ public/manejo_usuarios/js/login.js
@@ -1,33 +1,36 @@
+import { extractTokenFromHeader, setCookie } from "./cookieManager";
 
-const email = document.getElementById('email');
-const password = document.getElementById('password');
+const email = document.getElementById("email");
+const password = document.getElementById("password");
 
-login.addEventListener('submit', async (event) => {
-    event.preventDefault();
+login.addEventListener("submit", async (event) => {
+  event.preventDefault();
 
-    console.log(email)
-    const jsonData = {
-        email,
-        password,
-    };
+  console.log(email);
+  const jsonData = {
+    email,
+    password,
+  };
 
-    try {
-        const response = fetch('http://localhost:3000/usuario/logIn', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
+  try {
+    const response = await fetch("/logging", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(jsonData),
+    });
 
-            },
-            body: JSON.stringify(jsonData),
-        });
-
-        if (response.ok) {
-            console.log('holis')
-        } else {
-
-        }
-    } catch (error) {
-
-        console.error('Error submitting login form:', error);
+    if (response.ok) {
+      const token = extractTokenFromHeader(
+        response.headers.get("Authorization")
+      );
+      setCookie("webToken", token, 1);
+      window.location.href = "/";
+    } else {
+      console.error("Error submitting login form:", response);
     }
-})
+  } catch (error) {
+    console.error("Error submitting login form:", error);
+  }
+});
