aeee7b2a0398256355714d6410c8d3e87e93b48a
jreyess11 <josegabrielreyessolanojr@gmail.com>
2024-03-27 18:21:06 -0500

    commit

--- controllers/vitrina_productos/testing.controllers.js
+++ controllers/vitrina_productos/testing.controllers.js
@@ -31,20 +31,39 @@ export const defaultR3 = (req, res) => {
   res.render("user_review");
 };
 
+// export const defaultR4 = async (req, res) => {
+//   try {
+//     const query = req.query;
+//     const params = new URLSearchParams(query).toString();
+//     const response = await fetch(`http://localhost:1234/inventario/cards?${params}`);
+//     const datos = await response.json();
+    
+//     datos.forEach((dato) => {
+//       dato.imagePath = "vitrina_productos/img/cedric.jpg";
+//     });
+    
+//     res.render("vitrina_productos/index", { datos });
+//   } catch (error) {
+//     console.error("Error al obtener datos filtrados:", error);
+//     res.render("error", { message: "Error al obtener datos filtrados" });
+//   }
+// }
+
 export const defaultR4 = async (req, res) => {
   try {
     const query = req.query;
     const params = new URLSearchParams(query).toString();
     const response = await fetch(`http://localhost:1234/inventario/cards?${params}`);
     const datos = await response.json();
-    
+
     datos.forEach((dato) => {
-      dato.imagePath = "vitrina_productos/img/cedric.jpg";
+      dato.imagePath = "vitrina_productos/img/cedric.jpg"; 
     });
-    
+
     res.render("vitrina_productos/index", { datos });
+
   } catch (error) {
-    console.error("Error al obtener datos filtrados:", error);
-    res.render("error", { message: "Error al obtener datos filtrados" });
+    console.error(error);
+    res.status(500).send("Internal Server Error");
   }
-}
+};
