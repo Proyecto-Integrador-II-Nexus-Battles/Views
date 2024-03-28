8c58c175fcb9897d65457b27b0d88ea957d4fa8f
kintanuki <juandiquintanareyes@gmail.com>
2024-03-28 11:07:46 -0500

    variables_entorno

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
 import path from "path";
 import { fileURLToPath } from "url";
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export const PORT = 5000;
+// export const PORT = 5000;
+export const APP_PORT = process.env.APP_PORT;
+export const HOST = process.env.HOST;
+export const HOST_PORT = process.env.HOST_PORT;
