f1096b5c03851b27957a90b7f4b31e1d99ef60b8
Sofía princess <sofusaka@gmail.com>
2024-04-03 07:08:51 -0500

    v1 sofiA
    cambios en micuenta

--- routes/vitrina_productos/testing.routes.js
+++ routes/vitrina_productos/testing.routes.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
-import { defaultR, defaultR2, defaultR3, defaultR4, defaultR5, defaultR6, defaultR7,defaultR8} from "../../controllers/vitrina_productos/testing.controllers.js";
+import { defaultR, defaultR2, defaultR3, defaultR4, defaultR5, defaultR6, defaultR7 } from "../../controllers/vitrina_productos/testing.controllers.js";
 
 const router = Router();
 router.get("/", defaultR);
 router.get("/vistaDetallada/:id", defaultR2);
 router.get("/filteredCards/", defaultR4);
+router.get("/miCuenta", defaultR3)
 router.post("/vitrina/enviarCarro", defaultR5);
 router.post("/vitrina/enviarListaDeseos", defaultR6);
 router.post("/vitrina/eliminarListaDeseos", defaultR7);
-router.post("/vitrina/miCuenta", defaultR8)
+//router.post("/vitrina/miCuenta", defaultR8)
 
 
 export default router;
