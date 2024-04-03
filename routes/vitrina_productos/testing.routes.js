a8986b59b88a546bff144a604fc724f55cf0cc10
Andres <Olaya4423@gmail.com>
2024-04-03 06:58:11 -0500

    arreglos

--- routes/vitrina_productos/testing.routes.js
+++ routes/vitrina_productos/testing.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { defaultR, defaultR2, defaultR3, defaultR4, defaultR5, defaultR6, defaultR7} from "../../controllers/vitrina_productos/testing.controllers.js";
+import { defaultR, defaultR2, defaultR3, defaultR4, defaultR5, defaultR6, defaultR7,defaultR8} from "../../controllers/vitrina_productos/testing.controllers.js";
 
 const router = Router();
 router.get("/", defaultR);
@@ -8,6 +8,7 @@ router.get("/filteredCards/", defaultR4);
 router.post("/vitrina/enviarCarro", defaultR5);
 router.post("/vitrina/enviarListaDeseos", defaultR6);
 router.post("/vitrina/eliminarListaDeseos", defaultR7);
+router.post("/vitrina/miCuenta", defaultR8)
 
 
 export default router;
