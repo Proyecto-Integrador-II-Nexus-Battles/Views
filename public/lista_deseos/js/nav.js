e726c22cf43169fe78084fbd71242cc68e74b214
Santiago Villabona <santienrike16anime@gmail.com>
2024-04-07 12:04:58 -0500

    NUEVA NAVBAR

--- public/lista_deseos/js/nav.js
+++
@@ -1,9 +0,0 @@
-/* const boton_abrir = document.querySelector('.abrir-btn')
-const navbar = document.getElementById('navbar')
-
-boton_abrir.addEventListener('click',abrirBoton);
-function abrirBoton(){
-  navbar.classList.toggle('visible');
-
-}
- */
