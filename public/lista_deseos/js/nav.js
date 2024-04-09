1797f4cadad943f2bde77a6f10f88f95f264e371
David Cárdenas <107714070+davopaco@users.noreply.github.com>
2024-04-09 13:38:00 -0500

    Revert "NUEVA NAVBAR"

---
+++ public/lista_deseos/js/nav.js
@@ -0,0 +1,9 @@
+/* const boton_abrir = document.querySelector('.abrir-btn')
+const navbar = document.getElementById('navbar')
+
+boton_abrir.addEventListener('click',abrirBoton);
+function abrirBoton(){
+  navbar.classList.toggle('visible');
+
+}
+ */
