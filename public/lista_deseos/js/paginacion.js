aa69aa8343a4e3a61a2781ce36f0d74c0145817e
David Cárdenas <santito500@live.com>
2024-04-09 13:46:45 -0500

    Revert "Merge pull request #40 from Proyecto-Integrador-II-Nexus-Battles/revert-39-lista_deseos"
    This reverts commit c214d496102647884a436d696c86c6e888573ed8, reversing
    changes made to f46db6e5cf75990a55851789ccf4b2dc227a3695.

--- public/lista_deseos/js/paginacion.js
+++ public/lista_deseos/js/paginacion.js
@@ -1,49 +1,61 @@
 let thisPage = 1;
 let limit = 6;
-let list = document.querySelectorAll('.list .card');
+let list = []; // Inicializa la variable list como un array vacío
 
-function loadItem(){
+function loadItem() {
     let beginGet = limit * (thisPage - 1);
     let endGet = limit * thisPage - 1;
-    list.forEach((item, key)=>{
-        if(key >= beginGet && key <= endGet){
+
+    // Seleccionar todos los elementos de la clase .card
+    list = document.querySelectorAll('.right-side .card');
+
+    list.forEach((item, key) => {
+        if (key >= beginGet && key <= endGet) {
             item.style.display = 'block';
-        }else{
+        } else {
             item.style.display = 'none';
         }
-    })
+    });
+
     listPage();
 }
-loadItem();
-function listPage(){
+
+loadItem(); // Llama a la función loadItem() para que se ejecute al cargar la página
+
+function listPage() {
     let count = Math.ceil(list.length / limit);
     document.querySelector('.listPage').innerHTML = '';
 
-    if(thisPage != 1){
+    // Agregar la flecha de retroceso si no estamos en la primera página
+    if (thisPage != 1) {
         let prev = document.createElement('li');
-        prev.innerText = '<-';
+        prev.innerText = '←';
         prev.setAttribute('onclick', "changePage(" + (thisPage - 1) + ")");
         document.querySelector('.listPage').appendChild(prev);
     }
 
-    for(i = 1; i <= count; i++){
+    // Agregar los números de página
+    for (i = 1; i <= count; i++) {
         let newPage = document.createElement('li');
         newPage.innerText = i;
-        if(i == thisPage){
+        if (i == thisPage) {
             newPage.classList.add('active');
         }
         newPage.setAttribute('onclick', "changePage(" + i + ")");
         document.querySelector('.listPage').appendChild(newPage);
     }
 
-    if(thisPage != count){
+    // Agregar la flecha hacia adelante si no estamos en la última página
+    if (thisPage != count) {
         let next = document.createElement('li');
-        next.innerText = '->';
+        next.innerText = '→';
         next.setAttribute('onclick', "changePage(" + (thisPage + 1) + ")");
         document.querySelector('.listPage').appendChild(next);
     }
 }
-function changePage(i){
+
+
+function changePage(i) {
     thisPage = i;
     loadItem();
 }
