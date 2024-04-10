219408c533a2de9fb89eaf515fd92a6d89f06f9c
Andres <Olaya4423@gmail.com>
2024-04-10 16:22:07 -0500

    a

---
+++ public/subasta/js/emergentes.js
@@ -0,0 +1,24 @@
+const compraRapida = document.querySelector('.compra_rapida_container')
+const pujaContainer = document.querySelector('.puja_container')
+
+function abrirPujar(){
+if(pujaContainer.style.display = 'none'){
+  pujaContainer.style.display = 'flex'
+}
+}
+
+function abrirCompraRapida(){
+if(compraRapida.style.display = 'none'){
+compraRapida.style.display = 'flex'
+}
+}
+function cerrarEmer(){
+
+  if(pujaContainer.style.display = 'flex'){
+    pujaContainer.style.display ='none'
+  } else if(compraRapida.style.display = 'flex'){
+    compraRapida.style.display = 'none'
+  }
+}
+
+
