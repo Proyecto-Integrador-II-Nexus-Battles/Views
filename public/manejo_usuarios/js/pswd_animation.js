dd8090104e754ab125590f0abcb0d776a19d9fd5
TiltRex <afls2002@gmail.com>
2024-04-10 12:42:59 -0500

    correcciones

---
+++ public/manejo_usuarios/js/pswd_animation.js
@@ -0,0 +1,18 @@
+function toggleVisibility() {
+    var eye = document.getElementById('eye');
+    var show = document.getElementById('show');
+    var passwordInput = document.getElementById('password');
+
+    if (this.id === 'eye') {
+        eye.style.display = 'none';
+        show.style.display = 'inline';
+        passwordInput.type = 'text';
+    } else if (this.id === 'show') {
+        eye.style.display = 'inline';
+        show.style.display = 'none';
+        passwordInput.type = 'password';
+    }
+}
+
+document.getElementById('eye').addEventListener('click', toggleVisibility);
+document.getElementById('show').addEventListener('click', toggleVisibility);
