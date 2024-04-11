d493798f644e7a4187f23af4b9a02312d99d564f
DREYGON21 <83626661+NRicardoG@users.noreply.github.com>
2024-04-11 04:42:00 -0500

    Se realiza la creacion de mensajes de compra y venta, asi como detalle de compra y venta version:1.0

--- public/subasta/js/emergentes.js
+++ public/subasta/js/emergentes.js
@@ -22,4 +22,29 @@ function cerrarEmer(){
   }
 }
 
+// js modal pop ud emergante
+const btn_AbrirModal_01 = document.querySelector("#btn_AbrirModal_01");
+const btn_AbrirModal_02 = document.querySelector("#btn_AbrirModal_02");
+const Cerrar_Modal_01 = document.querySelector(".Cerrar_Modal_01");
+const Cerrar_Modal_02 = document.querySelector(".Cerrar_Modal_02");
+const Detalles_Modal_01 = document.querySelector(".Detalles_Modal_01");
+const Detalles_Modal_02 = document.querySelector(".Detalles_Modal_02");
+const modal_01 = document.querySelector(".modal_01");
+const modal_02 = document.querySelector(".modal_02");
 
+// abrir modales de venta y compra
+btn_AbrirModal_01.addEventListener("click",() =>{
+  modal_01.showModal();
+})
+btn_AbrirModal_02.addEventListener("click",() =>{
+  modal_02.showModal();
+})
+// cerrar modal
+Cerrar_Modal_01.addEventListener("click",() =>{
+  modal_01.close()
+})
+Cerrar_Modal_02.addEventListener("click",() =>{
+  modal_02.close()
+})
+
+// detalles modal
