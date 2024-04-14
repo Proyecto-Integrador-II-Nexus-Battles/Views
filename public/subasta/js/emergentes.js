b80db1b27b5d78e8391aeedb7ba361939db3a4a9
DREYGON21 <83626661+NRicardoG@users.noreply.github.com>
2024-04-14 18:11:32 -0500

    responsive y funcionalidad modal descripción del buzon

--- public/subasta/js/emergentes.js
+++ public/subasta/js/emergentes.js
@@ -31,6 +31,8 @@ const Detalles_Modal_01 = document.querySelector(".Detalles_Modal_01");
 const Detalles_Modal_02 = document.querySelector(".Detalles_Modal_02");
 const modal_01 = document.querySelector(".modal_01");
 const modal_02 = document.querySelector(".modal_02");
+const modal_Detalles_01 = document.querySelector(".modal_Detalles_01");
+const Cerrar_Modal_Detalle = document.querySelector(".Cerrar_Modal_Detalle");
 
 // abrir modales de venta y compra
 btn_AbrirModal_01.addEventListener("click",() =>{
@@ -49,4 +51,17 @@ Cerrar_Modal_02.addEventListener("click",() =>{
 })
 
 // detalles modal
+Detalles_Modal_01.addEventListener("click",() =>{
+  modal_01.close()
+  modal_Detalles_01.showModal()
+})
+
+Detalles_Modal_02.addEventListener("click",() =>{
+  modal_02.close()
+  modal_Detalles_01.showModal()
+})
+
+Cerrar_Modal_Detalle.addEventListener("click",()=>{
+  modal_Detalles_01.close()
+});
 
