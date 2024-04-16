f3f193d5bd959020cf2a692f50550b68dface480
Alexa020204 <alexa020204@gmail.com>
2024-04-16 11:25:21 -0500

    Arreglos del filtro

--- public/vitrina_productos/js/filtrarCartas.js
+++ public/vitrina_productos/js/filtrarCartas.js
@@ -1,32 +1,20 @@
 function filtrarCartas() {
   const filtros = {}; // donde se almacenaran los filtros seleccionados
-
-  const sortOrder = document.querySelector('select[name="sortOrder"]').value;
-  const min_max = document.querySelector('select[name="min_max"]').value;
-  const [min, max] = min_max.split('-');
-  const Type = document.querySelector('select[name="Type"]').value;
-  const sale = document.querySelector('input[name="sale"]').checked;
-
-  if (sortOrder !== "") {
+  // const sortOrder = document.querySelector('select[name="sortOrder"]').value;
+  const min = document.getElementById('min_price').value;
+  const max = document.getElementById('max_price').value;
+  const Type = document.getElementById('Type').value;
+  const sale = document.getElementById('sale').checked;
+  if (sortOrder !== "" && min !== "" && max !== "" && Type !== "") {
     filtros.sortOrder = sortOrder;
-  }
-  if (min_max !== "") {
     filtros.minPrice = min;
     filtros.maxPrice = max;
-  }
-  if (Type !== "") {
     filtros.Type = Type;
-  }
-  if (sale) {
     filtros.sale = sale;
   }
-
   const queryParams = new URLSearchParams(filtros).toString();
-  console.log(queryParams);
   const url = '/filteredCards/?' + queryParams;
-
   window.location.href = url
-
 }
 
 function filtrarPorBusqueda(event) {
