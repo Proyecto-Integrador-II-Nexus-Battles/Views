3dbc95fd83340063e98cd23f568cd2743baeb975
Santiadi <disacamo43@gmail.com>
2024-04-19 14:55:22 -0500

    routes

--- controllers/subasta/testing.controller.js
+++ controllers/subasta/testing.controller.js
@@ -32,3 +32,56 @@ export const cartasSubasta = async (_req, res) => {
 
     }
 }
+
+export const fetchBuzon =async (data)=>{
+    try{
+        const response = await fetch(`${HOST}:${PORT}/subasta/buzon`,{
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        const datos = await response.json();
+        console.log(datos);
+        return datos
+    }catch(error){
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+}
+
+export const fetchClaim = async (data)=>{
+    try{
+        const response = await fetch(`${HOST}:${PORT}/subasta/buzon/claim`,{
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        const datos = await response.json();
+        console.log(datos);
+        return datos
+    }catch(error){
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+}
+export const fetchAdd=async (data) => {
+    try{
+        const response = await fetch(`${HOST}:${PORT}/subasta/buzon/add`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        const datos = await response.json();
+        console.log(datos);
+        return datos
+    }catch(error){
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+}
