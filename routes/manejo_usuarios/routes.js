cc8d91862aab0f2e6141a0f476926d1433be0a61
David Cárdenas <santito500@live.com>
2024-04-23 16:16:25 -0500

    feat: :sparkles: Confirmación de cuenta y recuperación
    Ya se puede confirmar y recuperar la cuenta con el correo

--- routes/manejo_usuarios/routes.js
+++ routes/manejo_usuarios/routes.js
@@ -1,6 +1,27 @@
-import { userLogging, renderLogin, renderRegister, rendervitrina, irrvitrina, admin } from "../../controllers/manejo_usuarios/controller.js";
-import { renderAdmin, fetchLogin, buscarUsername, renderUser, fetchRegister } from "../../controllers/manejo_usuarios/controller.js";
+import {
+  userLogging,
+  renderLogin,
+  renderRegister,
+  rendervitrina,
+  irrvitrina,
+  admin,
+} from "../../controllers/manejo_usuarios/controller.js";
+import {
+  renderAdmin,
+  fetchLogin,
+  buscarUsername,
+  renderUser,
+  fetchRegister,
+} from "../../controllers/manejo_usuarios/controller.js";
 import { Router } from "express";
+import {
+  fetchNewPassword,
+  fetchRecuperarContrasena,
+  renderConfirmarCuenta,
+  renderNuevaContrasena,
+  renderRecuperarContrasena,
+  verifyCode,
+} from "../../controllers/manejo_usuarios/emailController.js";
 
 const router = new Router();
 
@@ -9,9 +30,15 @@ router.post("/logging", userLogging);
 router.get("/register", renderRegister);
 router.get("/vitrina", rendervitrina);
 router.get("/adminpage", renderAdmin);
-router.get('/buscar_user', buscarUsername);
+router.get("/buscar_user", buscarUsername);
 router.post("/usuario/login", fetchLogin);
 router.post("/usuario/register", fetchRegister);
 router.get("/user/:username", renderUser);
+router.post("/usuario/recover/send", fetchRecuperarContrasena);
+router.get("/usuario/recover", renderRecuperarContrasena);
+router.get("/usuario/recover/new", renderNuevaContrasena);
+router.patch("/usuario/recover/new", fetchNewPassword);
+router.post("/usuario/verify", verifyCode);
+router.get("/usuario/confirmar", renderConfirmarCuenta);
 
 export default router;
